f7ca97ea720901a33773394f45d5fed8d6e1671c
Hasna M.A.F <it18200034@my.sliit.lk>
2021-09-16 17:11:41 +0530

    api methods added

--- auth.js
+++ auth.js
@@ -1,14 +1,14 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
-// const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-// const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+// const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID; process.env.GOOGLE_CLIENT_ID,
+// const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;   process.env.GOOGLE_CLIENT_SECRET,
 const GOOGLE_CLIENT_ID = '306873896254-guod6t20jr65i6r13a47g3dsrfgobnn4.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = 'TdRiwx-5bl1k_J301NocG3P';
+const GOOGLE_CLIENT_SECRET = '3TdRiwx-5bl1k_J301NocG3P';
 
 passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
   callbackURL: "http://localhost:5000/auth/google/callback",
   passReqToCallback: true,
 },
