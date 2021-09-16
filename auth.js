b1dcd2c7cdbc1bef5c61c220a342b738f51fbed0
Hasna M.A.F <it18200034@my.sliit.lk>
2021-09-16 15:07:01 +0530

    added authorization part

---
+++ auth.js
@@ -0,0 +1,25 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+// const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+// const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const GOOGLE_CLIENT_ID = '306873896254-guod6t20jr65i6r13a47g3dsrfgobnn4.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = 'TdRiwx-5bl1k_J301NocG3P';
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:5000/auth/google/callback",
+  passReqToCallback: true,
+},
+function(request, accessToken, refreshToken, profile, done) {
+  return done(null, profile);
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
