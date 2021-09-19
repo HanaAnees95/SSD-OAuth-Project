a3bd07729d1385d97598ef2f26357463ca307424
Hasna M.A.F <it18200034@my.sliit.lk>
2021-09-19 10:41:13 +0530

    auth keys added

---
+++ configs/AuthKeys.js
@@ -0,0 +1,10 @@
+const AUTHKEYS = {
+    'googleOauth': {
+        'clientID': "306873896254-guod6t20jr65i6r13a47g3dsrfgobnn4.apps.googleusercontent.com",
+        'clientSecret': "3TdRiwx-5bl1k_J301NocG3P",
+        'callback': '/auth/google/redirect'
+    },
+    "session_key": "secret123"
+}
+
+module.exports = AUTHKEYS
